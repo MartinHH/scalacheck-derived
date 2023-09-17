a676817577a0eb55a3232a75a63382aabb7e7fb4
Martin Hansen <code@martin-hansen.de>
2023-09-17 17:05:16 +0200

    Revert "Update munit, munit-scalacheck to 1.0.0-M10"
    This reverts commit c83215afadf159011b62edd1941735166485c68b.

--- src/test/conf.scala
+++ src/test/conf.scala
@@ -1,3 +1,3 @@
 // must be defined here - see https://github.com/VirtusLab/scala-cli/issues/1729
-//> using lib "org.scalameta::munit::1.0.0-M10"
-//> using lib "org.scalameta::munit-scalacheck::1.0.0-M10"
+//> using lib "org.scalameta::munit::1.0.0-M8"
+//> using lib "org.scalameta::munit-scalacheck::1.0.0-M8"

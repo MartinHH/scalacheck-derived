efe725a710fd98d2c98d54903996b8cd461fbd38
Martin Hansen <code@martin-hansen.de>
2024-05-25 08:35:40 +0200

    chore: bump munit to 1.0.0

--- project.scala
+++ project.scala
@@ -3,8 +3,8 @@
 //> using jvm "8"
 
 //> using dep "org.scalacheck::scalacheck::1.18.0"
-//> using test.dep "org.scalameta::munit::1.0.0-M12"
-//> using test.dep "org.scalameta::munit-scalacheck::1.0.0-M12"
+//> using test.dep "org.scalameta::munit::1.0.0"
+//> using test.dep "org.scalameta::munit-scalacheck::1.0.0"
 
 //> using option "-Xfatal-warnings"
 //> using option "-Wunused:all"

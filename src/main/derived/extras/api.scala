ac7b6acbf8f27ab03ea7ea7b2937dfc28af6c62a
Martin Hansen <code@martin-hansen.de>
2024-06-15 14:10:33 +0200

    feat: Arbitrary for literal types

--- src/main/derived/extras/api.scala
+++ src/main/derived/extras/api.scala
@@ -1,3 +1,7 @@
 package io.github.martinhh.derived.extras
 
 object union extends UnionArbitraries
+
+object literal extends LiteralArbitraries
+
+object all extends LiteralArbitraries with UnionArbitraries

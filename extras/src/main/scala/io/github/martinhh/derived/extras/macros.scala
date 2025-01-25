7883bfb259bcdccc7b381e872d0df92c931ba4c3
Martin Hansen <code@martin-hansen.de>
2025-01-25 11:23:57 +0100

    refactor: rewrite macros a bit

--- extras/src/main/scala/io/github/martinhh/derived/extras/macros.scala
+++ extras/src/main/scala/io/github/martinhh/derived/extras/macros.scala
@@ -8,21 +8,20 @@ import scala.quoted.*
 // macros for union based on this StackOverflow answer by Dmytro Mitin:
 // https://stackoverflow.com/a/78567397/6152669
 
-private def unionGens[X: Type](using Quotes): Expr[UnionGens[X]] =
-  import quotes.reflect.*
+private def unionGens[X: Type](using q: Quotes): Expr[UnionGens[X]] =
+  import q.reflect.*
   TypeRepr.of[X] match
     case OrType(l, r) =>
       (l.asType, r.asType) match
         case ('[a], '[b]) =>
-          (Expr.summon[UnionGens[a]], Expr.summon[UnionGens[b]]) match
-            case (Some(aInst), Some(bInst)) =>
-              '{
-                val x = $aInst
-                val y = $bInst
-                UnionGens(x.gens ++ y.gens)
-              }.asExprOf[UnionGens[X]]
-            case (_, _) =>
-              report.errorAndAbort(s"Could not summon UnionGens")
+          val exprOpt: Option[Expr[UnionGens[X]]] =
+            for {
+              aGens <- Expr.summon[UnionGens[a]]
+              bGens <- Expr.summon[UnionGens[b]]
+            } yield '{
+              UnionGens($aGens.gens ++ $bGens.gens)
+            }.asExprOf[UnionGens[X]]
+          exprOpt.getOrElse(report.errorAndAbort(s"Could not summon UnionGens"))
     case x =>
       report.errorAndAbort(s"${x.show} is not a union type")
 
@@ -30,22 +29,21 @@ private transparent inline given unionGensMacro[X]: UnionGens[X] =
   ${ unionGens[X] }
 
 private def unionTypedTypeClasses[TC[_]: Type, X: Type](
-  using Quotes
+  using q: Quotes
 ): Expr[UnionTypedTypeClasses[TC, X]] =
-  import quotes.reflect.*
+  import q.reflect.*
   TypeRepr.of[X] match
     case OrType(l, r) =>
       (l.asType, r.asType) match
         case ('[a], '[b]) =>
-          (Expr.summon[TypedTypeClasses[TC, a]], Expr.summon[TypedTypeClasses[TC, b]]) match
-            case (Some(aInst), Some(bInst)) =>
-              '{
-                val x = $aInst
-                val y = $bInst
-                UnionTypedTypeClasses[TC, a | b](x.instances ++ y.instances)
-              }.asExprOf[UnionTypedTypeClasses[TC, X]]
-            case (_, _) =>
-              report.errorAndAbort(s"Could not summon instances for Union")
+          val exprOpt: Option[Expr[UnionTypedTypeClasses[TC, X]]] =
+            for {
+              aTTC <- Expr.summon[TypedTypeClasses[TC, a]]
+              bTTC <- Expr.summon[TypedTypeClasses[TC, b]]
+            } yield '{
+              UnionTypedTypeClasses[TC, a | b]($aTTC.instances ++ $bTTC.instances)
+            }.asExprOf[UnionTypedTypeClasses[TC, X]]
+          exprOpt.getOrElse(report.errorAndAbort(s"Could not summon instances for Union"))
     case x =>
       report.errorAndAbort(s"${x.show} is not a union type")
 

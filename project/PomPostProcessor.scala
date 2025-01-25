effa8326e6985a6f907602efdb176ed01e2d2943
Martin Hansen <code@martin-hansen.de>
2025-01-25 16:00:41 +0100

    build: refactor PomPostProcessor

--- project/PomPostProcessor.scala
+++ project/PomPostProcessor.scala
@@ -5,23 +5,34 @@ import scala.xml.transform.{RewriteRule, RuleTransformer}
 
 object PomPostProcessor {
 
+  private def isTestDependency(elem: Elem): Boolean = {
+    def isDependency: Boolean = elem.label == "dependency"
+    def isTestScopeOnly: Boolean = {
+      val scopes = elem.child.filter(_.label == "scope")
+      scopes.nonEmpty && scopes.forall(_.text == "test")
+    }
+    isDependency && isTestScopeOnly
+  }
+
   /**
-   * Helper for excluding scoped dependencies from the pom.xml.
+   * Helper for excluding test dependencies from the pom.xml.
    *
-   * Based on this StackOverflow-answer: [[https://stackoverflow.com/a/51416386]].
-   *
-   * (This is intended to ensure that munit is not listed as dependency in any form.)
+   * (This is intended to ensure that stuff like munit is not listed as dependency in any form.)
    */
-  def removeScopedDependencies(logger: Logger)(node: Node): Node =
+  def removeTestDependencies(logger: Logger)(node: Node): Node =
+    // Solution idea is based on this StackOverflow answer:
+    // https://stackoverflow.com/a/78567397/6152669
+    // (This is not intended to be a legally binding copyright attribution because applying an idea
+    // that was expressed in less than 15 lines of example code within similar code that only makes
+    // a few straightforward calls to a standard API is not legally significant.)
     new RuleTransformer(new RewriteRule {
       override def transform(node: Node): NodeSeq = node match {
-        case e: Elem
-            if e.label == "dependency" && e.child.exists(child => child.label == "scope") =>
+        case elem: Elem if isTestDependency(elem) =>
           def txt(label: String): String =
-            "\"" + e.child.filter(_.label == label).flatMap(_.text).mkString + "\""
-          logger.info(s"""scoped dependency ${txt("groupId")} % ${txt("artifactId")} % ${txt(
-              "version"
-            )} % ${txt("scope")} has been omitted """)
+            elem.child.filter(_.label == label).flatMap(_.text).mkString
+          def artifact: String =
+            s"${txt("groupId")} % ${txt("artifactId")} % ${txt("version")}"
+          logger.info(s"""test dependency "$artifact" has been omitted """)
           NodeSeq.Empty
         case _ => node
       }

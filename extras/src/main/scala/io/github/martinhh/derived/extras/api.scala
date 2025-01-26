395ce8cdca0efb30ba3d2b04db994d573094b7ad
Martin Hansen <code@martin-hansen.de>
2025-01-26 13:26:07 +0100

    refactor: extras api
    - make naming more consistent with existing ("core") api
    - add scaladoc
    - add another test case

--- extras/src/main/scala/io/github/martinhh/derived/extras/api.scala
+++ extras/src/main/scala/io/github/martinhh/derived/extras/api.scala
@@ -1,7 +1,36 @@
 package io.github.martinhh.derived.extras
 
-object union extends UnionArbitraries with UnionCogens with UnionShrinks
+object union:
 
-object literal extends LiteralArbitraries
+  /**
+   * Public "API-entry-point" for derivation of `Arbitrary`-instances only for union types.
+   */
+  object arbitrary extends UnionArbitraries
 
-object all extends LiteralArbitraries with UnionArbitraries with UnionCogens with UnionShrinks
+  /**
+   * Public "API-entry-point" for derivation of scalacheck-typeclass-instances for union types.
+   *
+   * This does not provide derivation of `Shrink`-instances as that might not always be desired.
+   * You can opt in to derivation of `Shrink`-instances via `shrink`.
+   */
+  object scalacheck extends UnionArbitraries with UnionCogens
+
+  /**
+   * Public "API-entry-point" for derivation of `Cogen`-instances only for union types.
+   */
+  object cogen extends UnionCogens
+
+  /**
+   * Public "API-entry-point" for derivation of `Shrink`-instances only for union types.
+   */
+  object shrink extends UnionShrinks
+
+object literal:
+  /**
+   * Public "API-entry-point" for derivation of `Arbitrary`-instances for literal types.
+   *
+   * (No derivation for `Shrink` is provided because there is no sense in shrinking single-value types.
+   * No derivation for `Cogen` is provided because the `Cogen` instances for the respective parent types
+   * should be sufficient.)
+   */
+  object arbitrary extends LiteralArbitraries

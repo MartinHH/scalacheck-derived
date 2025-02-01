217ac23249744b77022fd5d83aa05c76d8bf16f5
Martin Hansen <code@martin-hansen.de>
2025-02-01 20:19:52 +0100

    fix: ensure union-macros fail for non-union types
    (Also: reduce redundancies in union-macro.)

--- extras/src/main/scala/io/github/martinhh/derived/extras/union/TypedTypeClass.scala
+++ extras/src/main/scala/io/github/martinhh/derived/extras/union/TypedTypeClass.scala
@@ -10,18 +10,3 @@ private case class TypedTypeClass[TC[_], A](typeTest: TypeTest[Any, A], instance
 private object TypedTypeClass:
   inline given derived[TC[_], A]: TypedTypeClass[TC, A] =
     TypedTypeClass[TC, A](summonInline[TypeTest[Any, A]], summonInline[TC[A]])
-
-// type for accumulating TypedTypeClass instances (e.g. of a union)
-private sealed trait TypedTypeClasses[TC[_], A]:
-  def instances: List[TypedTypeClass[TC, ? <: A]]
-
-private object TypedTypeClasses:
-  inline given derived[TC[_], A]: SingleTypedTypeClasses[TC, A] =
-    SingleTypedTypeClasses(summonInline[TypedTypeClass[TC, A]])
-
-private case class SingleTypedTypeClasses[TC[_], A](instance: TypedTypeClass[TC, A])
-  extends TypedTypeClasses[TC, A]:
-  override def instances: List[TypedTypeClass[TC, A]] = List(instance)
-
-private case class UnionTypedTypeClasses[TC[_], A](instances: List[TypedTypeClass[TC, ? <: A]])
-  extends TypedTypeClasses[TC, A]

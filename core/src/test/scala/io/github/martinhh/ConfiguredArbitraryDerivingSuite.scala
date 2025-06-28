1a420130fcf022c8f791563e4b96dff583a91ee7
Martin Hansen <code@martin-hansen.de>
2025-06-28 07:23:01 +0200

    test: improve ConfiguredArbitraryDerivingSuite

--- core/src/test/scala/io/github/martinhh/ConfiguredArbitraryDerivingSuite.scala
+++ core/src/test/scala/io/github/martinhh/ConfiguredArbitraryDerivingSuite.scala
@@ -18,21 +18,25 @@ class ConfiguredArbitraryDerivingSuite extends test.ArbitrarySuite:
       config.fold(gens.head)(_.select(gens))
 
   test("custom conf via explicit call without conf in scope") {
-    equalArbitraryValues[ABC](Gen.const(ABC.A))(using confExample.deriveArbitrary)
+    equalArbitraryValues[Maybe[ABC]](Gen.const(Maybe.Defined(ABC.A)))(
+      using confExample.deriveArbitrary
+    )
   }
 
   test("custom conf via explicit call with conf in scope") {
     given Conf[ABC] = lastConf
-    equalArbitraryValues[ABC](Gen.const(ABC.C))(using confExample.deriveArbitrary)
+    equalArbitraryValues[Maybe[ABC]](Gen.const(Maybe.Defined(ABC.C)))(
+      using confExample.deriveArbitrary
+    )
   }
 
   test("custom conf via given import without conf in scope") {
     import confExample.given
-    equalArbitraryValues[ABC](Gen.const(ABC.A))
+    equalArbitraryValues[Maybe[ABC]](Gen.const(Maybe.Defined(ABC.A)))
   }
 
   test("custom conf via given import with conf in scope") {
     import confExample.given
     given Conf[ABC] = lastConf
-    equalArbitraryValues[ABC](Gen.const(ABC.C))
+    equalArbitraryValues[Maybe[ABC]](Gen.const(Maybe.Defined(ABC.C)))
   }
